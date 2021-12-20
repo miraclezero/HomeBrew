34c1121bb863c45c2cd2e0676744c14d3dfb071f
miraclezero <flmajia@gmail.com>
2021-12-20 16:45:59 -0600

    Update update_repo.sh

--- utility_script/update_repo.sh
+++ utility_script/update_repo.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-#cd /home/pi/openbehavior/ && rm -rf PeerPub/ && cd /home/pi/openbehavior/ && git clone https://www.github.com/nijie321/PeerPub.git
+##cd /home/pi/openbehavior/ && rm -rf PeerPub/ && cd /home/pi/openbehavior/ && git clone https://www.github.com/nijie321/PeerPub.git
 
 cd /home/pi/openbehavior/HomeBrew/ && git pull
 
 echo $(date +"%m-%d-%y %T") $(git log -1 --pretty=oneline) >> "/home/pi/SocialDrinking/${BOXID}_update"
 
 
-bash /home/pi/openbehavior/PeerPub/wifi-network/rsync.sh
+bash /home/pi/openbehavior/HomeBrew/wifi-network/rsync.sh
